d85f0b5812e07ada2c7d98966958a3b839135fd3
CKA <163610791+b01dc0de@users.noreply.github.com>
2024-11-15 13:19:16 -0600

    Switch background color via Enter

--- src/ArchDemos.cpp
+++ src/ArchDemos.cpp
@@ -3,25 +3,15 @@
 
 static bool bRunning = true;
 
-static void ErrorCallback(int ErrNo, const char* ErrDesc)
-{
-    OutputDebugString(ErrDesc);
-}
-
-static void KeyCallback(GLFWwindow* Window, int Key, int ScanCode, int Action, int Mods)
-{
-    if (Key == GLFW_KEY_ESCAPE && Action == GLFW_PRESS)
-    {
-        glfwSetWindowShouldClose(Window, GLFW_TRUE);
-    }
-}
-
 namespace Arch
 {
     int Width = 1280;
     int Height = 960;
     const char* WindowTitle = "ArchDemos";
     GLFWwindow* Window = nullptr;
+
+    static void ErrorCallback(int ErrNo, const char* ErrDesc);
+    static void KeyCallback(GLFWwindow* Window, int Key, int ScanCode, int Action, int Mods);
 
     static bool Init()
     {
@@ -54,6 +44,27 @@ namespace Arch
         return bResult;
     }
 
+    struct Color
+    {
+        float R;
+        float G;
+        float B;
+        float A;
+    };
+
+    static const Color BgColors[] = {
+        { 0.890f, 0.412f, 0.557f, 1.0f },
+        { 0.408f, 0.369f, 0.600f, 1.0f },
+        { 0.306f, 0.259f, 0.600f, 1.0f },
+        { 0.125f, 0.133f, 0.267f, 1.0f },
+        { 0.000f, 0.561f, 0.325f, 1.0f },
+        { 0.063f, 0.388f, 0.435f, 1.0f }
+    };
+
+#define ARRAY_SIZE(A) (sizeof(A) / sizeof(A[0]))
+    static const int NumBgColors = ARRAY_SIZE(BgColors);
+    static int BgIdx = 0;
+
     static bool MainLoop()
     {
         bool bResult = true;
@@ -61,15 +72,19 @@ namespace Arch
         bool bRunning = true;
         while (bRunning)
         {
-            glfwPollEvents();
+            { // Draw
+                int FrameWidth = 0, FrameHeight = 0;
+                glfwGetFramebufferSize(Window, &FrameWidth, &FrameHeight);
+                glViewport(0, 0, FrameWidth, FrameHeight);
+                glClear(GL_COLOR_BUFFER_BIT);
+                glClearColor(BgColors[BgIdx].R, BgColors[BgIdx].G, BgColors[BgIdx].B, 1.0f);
+                glfwSwapBuffers(Window);
+            }
 
-            int FrameWidth = 0, FrameHeight = 0;
-            glfwGetFramebufferSize(Window, &FrameWidth, &FrameHeight);
-            glViewport(0, 0, FrameWidth, FrameHeight);
-            glClear(GL_COLOR_BUFFER_BIT);
-            glfwSwapBuffers(Window);
-
-            bRunning = !glfwWindowShouldClose(Window);
+            { // Update
+                glfwPollEvents();
+                bRunning = !glfwWindowShouldClose(Window);
+            }
         }
 
         return bResult;
@@ -83,6 +98,23 @@ namespace Arch
         glfwTerminate();
 
         return bResult;
+    }
+
+    static void ErrorCallback(int ErrNo, const char* ErrDesc)
+    {
+        OutputDebugString(ErrDesc);
+    }
+
+    static void KeyCallback(GLFWwindow* Window, int Key, int ScanCode, int Action, int Mods)
+    {
+        if (Key == GLFW_KEY_ESCAPE && Action == GLFW_PRESS)
+        {
+            glfwSetWindowShouldClose(Window, GLFW_TRUE);
+        }
+        else if (Key == GLFW_KEY_ENTER && Action == GLFW_PRESS)
+        {
+            BgIdx = (BgIdx + 1) % NumBgColors;
+        }
     }
 }
 
