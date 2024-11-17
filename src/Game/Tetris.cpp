a0d76258d1ff99eaaafe9c57cd8bc4e9c4766149
CKA <163610791+b01dc0de@users.noreply.github.com>
2024-11-17 14:47:52 -0600

    Begin implementing Tetris game code

--- src/Game/Tetris.cpp
+++ src/Game/Tetris.cpp
@@ -4,5 +4,33 @@ namespace Arch
 {
     namespace Tetris
     {
+        enum TetrominoType 
+        {
+            BLOCK_EMPTY,
+            BLOCK_I,
+            BLOCK_J,
+            BLOCK_L,
+            BLOCK_O,
+            BLOCK_S,
+            BLOCK_Z,
+            BLOCK_T,
+            BLOCK_NUM
+
+        static const int GridWidth = 10;
+        static const int GridHeight = 20;
+        static const int GridSize = GridWidth * GridHeight;
+        TetrominoType Grid[GridSize];
+
+        void Init()
+        {
+            for (int GridIdx = 0; GridIdx < GridSize; GridIdx++)
+            {
+                Grid[GridIdx] = BLOCK_EMPTY;
+            }
+        }
+
+        void DrawGrid()
+        {
+        }
     }
 }
