73cc1f91443e598347040d12bdf6a87acc30310c
CKA <163610791+b01dc0de@users.noreply.github.com>
2024-11-20 18:35:50 -0600

    Switch mag filter to nearest for fallback texture

--- src/Graphics.cpp
+++ src/Graphics.cpp
@@ -188,7 +188,7 @@ namespace Arch
                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
                 glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, TestTextureWidth, TestTextureHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, TextureData);
                 glGenerateMipmap(GL_TEXTURE_2D);
